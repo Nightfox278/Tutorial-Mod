501c67376c90c4c796f24f85784375b4ca98b186
BuildTools <unconfigured@null.spigotmc.org>
2017-02-19 12:31:50 -0500

    Working?

---
+++ src/main/java/uk/co/weahavecookies56/tutorialmod/proxy/ClientProxy.java
@@ -0,0 +1,29 @@
+package uk.co.weahavecookies56.tutorialmod.proxy;
+
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import uk.co.weahavecookies56.tutorialmod.item.ModItems;
+
+/**
+ * Created by Toby on 18/08/2016.
+ */
+public class ClientProxy extends CommonProxy {
+
+    @Override
+    public void preInit(FMLPreInitializationEvent event) {
+
+        ModItems.registerRenders();
+
+    }
+
+    @Override
+    public void init(FMLInitializationEvent event) {
+
+    }
+
+    @Override
+    public void postInit(FMLPostInitializationEvent event) {
+
+    }
+}
